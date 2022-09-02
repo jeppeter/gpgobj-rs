6a3a9079bb1e39c34231b26fa301adc160a8b537
jeppeter <jeppeter@gmail.com>
2022-09-02 12:51:20 +0800

    to add gpgobj logger

--- gpgobj/src/logger.rs
+++ gpgobj/src/logger.rs
@@ -1,8 +1,9 @@
 
+
 use std::env;
 use std::io::{Write};
 use std::fs;
-//use std::io::prelude::*;
+
 use lazy_static::lazy_static;
 use chrono::{Local,Timelike,Datelike};
 
@@ -19,7 +20,6 @@ fn _gpgobj_get_environ_var(envname :&str) -> String {
 	}
 }
 
-#[allow(dead_code)]
 struct LogVar {
 	level :i32,
 	nostderr : bool,
@@ -27,7 +27,8 @@ struct LogVar {
 }
 
 
-fn gpgobj_proc_log_init(prefix :&str) -> LogVar {
+
+fn gpgobj_macro_log_init(prefix :&str) -> LogVar {
 	let mut getv :String;
 	let mut retv :i32 = 0;
 	let mut nostderr :bool = false;
@@ -61,7 +62,7 @@ fn gpgobj_proc_log_init(prefix :&str) -> LogVar {
 	if getv.len() > 0 {
 		let fo = fs::File::create(&getv);
 		if fo.is_err() {
-			eprintln!("can not open [{}]", getv);
+			eprintln!("can not open [{}]", getv);		
 		} else {
 			coptfile = Some(fo.unwrap());
 		}
@@ -74,15 +75,13 @@ fn gpgobj_proc_log_init(prefix :&str) -> LogVar {
 	};
 }
 
-
 lazy_static! {
 	static ref GPGOBJ_LOG_LEVEL : LogVar = {
-		gpgobj_proc_log_init("GPGOBJ")
+		gpgobj_macro_log_init("GPGOBJ")
 	};
 }
 
 
-#[allow(dead_code)]
 pub (crate)  fn gpgobj_debug_out(level :i32, outs :&str) {
 	if GPGOBJ_LOG_LEVEL.level >= level {
 		let c = format!("{}\n",outs);
@@ -98,156 +97,44 @@ pub (crate)  fn gpgobj_debug_out(level :i32, outs :&str) {
 	return;
 }
 
-#[allow(dead_code)]
+
 pub (crate) fn gpgobj_log_get_timestamp() -> String {
 	let now = Local::now();
 	return format!("{}/{}/{} {}:{}:{}",now.year(),now.month(),now.day(),now.hour(),now.minute(),now.second());
 }
 
 
-#[macro_export]
 macro_rules! gpgobj_log_error {
 	($($arg:tt)+) => {
 		let mut c :String= format!("<ERROR>{}[{}:{}]  ",gpgobj_log_get_timestamp(),file!(),line!());
 		c.push_str(&(format!($($arg)+)[..]));
-		asn1obj_debug_out(0,&c);
+		gpgobj_debug_out(0,&c);
 	}
 }
 
-#[macro_export]
+#[allow(unused_macros)]
 macro_rules! gpgobj_log_warn {
 	($($arg:tt)+) => {
 		let mut c :String= format!("<WARN>{}[{}:{}]  ",gpgobj_log_get_timestamp(),file!(),line!());
 		c.push_str(&(format!($($arg)+)[..]));
-		asn1obj_debug_out(10,&c);
+		gpgobj_debug_out(10,&c);
 	}
 }
 
 
-#[macro_export]
+#[allow(unused_macros)]
 macro_rules! gpgobj_log_info {
 	($($arg:tt)+) => {
 		let mut c :String= format!("<INFO>{}[{}:{}]  ",gpgobj_log_get_timestamp(),file!(),line!());
 		c.push_str(&(format!($($arg)+)[..]));
-		asn1obj_debug_out(20,&c);
+		gpgobj_debug_out(20,&c);
 	}
 }
 
-#[macro_export]
 macro_rules! gpgobj_log_trace {
 	($($arg:tt)+) => {
 		let mut _c :String= format!("<TRACE>{}[{}:{}]  ",gpgobj_log_get_timestamp(),file!(),line!());
 		_c.push_str(&(format!($($arg)+)[..]));
-		asn1obj_debug_out(40, &_c);
-	}
-}
-
-
-#[macro_export]
-macro_rules! gpgobj_assert {
-	($v:expr , $($arg:tt)+) => {
-		if !($v) {
-			let mut _c :String= format!("[{}:{}] ",file!(),line!());
-			_c.push_str(&(format!($($arg)+)[..]));
-			panic!("{}", _c);
-		}
-	}
-}
-
-
-#[macro_export]
-macro_rules! gpgobj_format_buffer_log {
-	($buf:expr,$len:expr,$info:tt,$iv:expr,$($arg:tt)+) => {
-		let mut c :String = format!("[{}:{}]",file!(),line!());
-		c.push_str(&format!("{} ",$info));
-		c.push_str(&gpgobj_log_get_timestamp());
-		c.push_str(": ");
-		c.push_str(&(format!($($arg)+)[..]));
-		let _ptr :*const u8 = $buf as *const u8;
-		let  mut _ci :usize;
-		let _totallen: usize = $len as usize;
-		let mut _lasti :usize = 0;
-		let mut _nb :u8;
-		c.push_str(&format!(" buffer [{:?}][{}]",_ptr,_totallen));
-		_ci = 0;
-		while _ci < _totallen {
-			if (_ci % 16) == 0 {
-				if _ci > 0 {
-					c.push_str("    ");
-					while _lasti < _ci {
-						unsafe{
-							_nb = *_ptr.offset(_lasti as isize);	
-						}
-						
-						if _nb >= 0x20 && _nb <= 0x7e {
-							c.push(_nb as char);
-						} else {
-							c.push_str(".");
-						}
-						_lasti += 1;
-					}
-				}
-				c.push_str(&format!("\n0x{:08x}:", _ci));
-			}
-			unsafe {_nb = *_ptr.offset(_ci as isize);}			
-			c.push_str(&format!(" 0x{:02x}",_nb));
-			_ci += 1;
-		}
-
-		if _lasti < _ci {
-			while (_ci % 16) != 0 {
-				c.push_str("     ");
-				_ci += 1;
-			}
-
-			c.push_str("    ");
-
-			while _lasti < _totallen {
-				unsafe {_nb = *_ptr.offset(_lasti as isize);}				
-				if _nb >= 0x20 && _nb <= 0x7e {
-					c.push(_nb as char);
-				} else {
-					c.push_str(".");
-				}
-				_lasti += 1;
-			}
-			//c.push_str("\n");
-		}
-		gpgobj_debug_out($iv,&c);
-	}
-}
-
-#[macro_export]
-macro_rules! gpgobj_debug_buffer_error {
-	($buf:expr,$len:expr,$($arg:tt)+) => {
-		gpgobj_format_buffer_log!($buf,$len,"<ERROR>",0,$($arg)+);
-	}
-}
-
-#[macro_export]
-macro_rules! gpgobj_debug_buffer_warn {
-	($buf:expr,$len:expr,$($arg:tt)+) => {
-		gpgobj_format_buffer_log!($buf,$len,"<WARN>",1,$($arg)+);
-	}
-}
-
-#[macro_export]
-macro_rules! gpgobj_debug_buffer_info {
-	($buf:expr,$len:expr,$($arg:tt)+) => {
-		gpgobj_format_buffer_log!($buf,$len,"<INFO>",2,$($arg)+);
-	}
-}
-
-#[macro_export]
-macro_rules! gpgobj_debug_buffer_debug {
-	($buf:expr,$len:expr,$($arg:tt)+) => {
-		gpgobj_format_buffer_log!($buf,$len,"<DEBUG>",3,$($arg)+);
-	}
-}
-
-#[macro_export]
-macro_rules! gpgobj_debug_buffer_trace {
-	($buf:expr,$len:expr,$($arg:tt)+) => {
-		gpgobj_format_buffer_log!($buf,$len,"<TRACE>",4,$($arg)+);
+		gpgobj_debug_out(40, &_c);
 	}
 }
